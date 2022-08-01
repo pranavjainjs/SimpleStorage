8f5f86e61538c04d0b6e6189dddc12e923d81e67
Pranav <95164530+thefakepranav@users.noreply.github.com>
2022-08-01 07:32:48 +0530

    one commit

--- deploy.py
+++ deploy.py
@@ -7,3 +7,21 @@ from solcx import compile_standard, install_solc
 with open("./SimpleStorage.sol", "r") as file:
     simple_storage_file = file.read()
 
+print("Installing...")
+install_solc("0.6.0")
+
+compiled_sol = compile_standard(
+    {
+        "language": "Solidity",
+        "sources": {"SimpleStorage.sol": {"content": simple_storage_file}},
+        "settings": {
+            "outputSelection": {
+                "*": {
+                    "*": ["abi", "metadata", "evm.bytecode", "evm.bytecode.sourceMap"]
+                }
+            }
+        },
+    },
+    solc_version="0.6.0",
+)
+
